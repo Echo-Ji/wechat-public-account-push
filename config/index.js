d18cf408e504b90304d15503de188a2623d2eca8
Echo <bh1506jjh@163.com>
2022-08-27 14:06:21 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,28 +1,23 @@
 export const config = {
     // 公众号配置
     // 公众号appId
-    appId: "",
+    appId: "wx744af6fc01f09335",
     // 公众号appSecret
-    appSecret: "",
+    appSecret: "442f9d06ffaf39fbad58ca52058fb18c",
     // 模板消息id
-    templateId: "",
+    templateId: "gac40uOdreS5YwLgGuYv1M8Rf4ADUcKmIK1XbMYGz34",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    user: [],
+    user: ["o1DOp5peYKJzdlh8zTagiLDVYJrU"],
      
     // 信息配置
     // 所在省份
-    province: "广东",
+    province: "北京",
     // 所在城市
-    city: "惠州",
+    city: "北京",
     // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
-    birthdays: [
-      {"name": "老婆", "year": "1993", "date": "12-27", "type": 'new'},
-      {"name": "家公", "year": "1993", "date": "08-09", "type": 'new'},
-    ],
+    birthday_message: "2022-10-12",
     // 在一起的日子，格式同上
-    loveDate: "2015-05-01",
-    // 结婚纪念日
-    marryDate: "2020-01-04"
+    love_day: "2020-01-22",
     }
 
 // {{date.DATA}}  
